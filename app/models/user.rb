493fa0d7081ea8f373c331e560d5866f192ec5b3
fulltostack <fulltostack@gmail.com>
2019-07-19 18:00:43 +0530

    added associations and validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,6 @@
 class User < ApplicationRecord
   has_secure_password
+  has_many :youtube_videos, dependent: :destroy, inverse_of: :user
+  has_many :youtube_video_votes, dependent: :destroy, inverse_of: :user
+  validates :email, presence: true, uniqueness: true, format: URI::MailTo::EMAIL_REGEXP
 end
--- app/models/youtube_video.rb
+++ app/models/youtube_video.rb
@@ -1,3 +1,5 @@
 class YoutubeVideo < ApplicationRecord
   belongs_to :user
+  has_many :youtube_video_votes, dependent: :destroy
+  validates :url, presence: true, format: /\A.*(youtu.be\/|v\/|u\/\w\/|embed\/|watch\?v=|\&v=)([^#\&\?]*).*/i.freeze
 end
